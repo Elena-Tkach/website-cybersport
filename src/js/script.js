34b039ce0078ab8b290e9a69fe419fc98aa0050e
Elena <tcachelena@gmail.com>
2021-07-08 09:50:09 +0300

    added page service

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,65 @@
-const body = document.body;
+window.addEventListener(`DOMContentLoaded`, () => {
+
+    const body = document.body;
+
+    const tabs = document.querySelectorAll(`.js-payingItem`);
+    const tabsContent = document.querySelectorAll(`.js-serviceItem`);
+    const tabParent = document.querySelector(`.js-payingList`)
+
+    hideTabContent();
+    showTabContent();
+
+    tabParent.addEventListener(`click`, (event) => {
+        const target = event.target;
+        if (target && target.classList.contains(`js-payingItem`)) {
+            tabs.forEach((item, i) => {
+
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    })
 
 
-//@@include('_burger.js');
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add(`hide`);
+            item.classList.remove(`active`);
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(`show`);
+        })
+
+
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add(`active`);
+        tabsContent[i].classList.remove(`hide`);
+        tabs[i].classList.add(`show`);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
