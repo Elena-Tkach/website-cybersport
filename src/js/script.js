477ae5ab325af0108aaa326bfe4366eb366a506e
Elena <tcachelena@gmail.com>
2021-07-11 16:45:43 +0300

    header

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,90 @@
+
+
 window.addEventListener(`DOMContentLoaded`, () => {
 
     const body = document.body;
 
-    const tabs = document.querySelectorAll(`.js-payingItem`);
-    const tabsContent = document.querySelectorAll(`.js-serviceItem`);
-    const tabParent = document.querySelector(`.js-payingList`)
+    const menu = document.querySelector(`.js-menu`);
+    const burgerBtn = document.querySelector(`.js-burger`);
 
-    hideTabContent();
-    showTabContent();
+    burgerBtn.addEventListener(`click`, showMenuBtnClick);
 
-    tabParent.addEventListener(`click`, (event) => {
-        const target = event.target;
-        if (target && target.classList.contains(`js-payingItem`)) {
-            tabs.forEach((item, i) => {
 
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            })
-        }
+    function showMenuBtnClick() {
+        menu.classList.toggle(`show`);
+    }
+
+
+
+    const dropdownItem = document.querySelector(`.js-dropdown`);
+    const dropsoqnFooter = document.querySelector(`.js-dropdown-footer`);
+
+
+    dropdownItem.forEach(item => {
+        item.addEventListener(`click`, openNavList);
+    })
+
+    dropdownItem.addEventListener(`click`, openNavList);
+
+    dropdownItem.addEventListener(`mouseover`, () => {
+        dropdownItem.classList.add(`open-list`);
+    });
+
+    dropdownItem.addEventListener(`mouseout`, () => {
+        dropdownItem.classList.remove(`open-list`);
     })
 
 
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add(`hide`);
-            item.classList.remove(`active`);
-        });
+    function openNavList() {
 
-        tabs.forEach(item => {
-            item.classList.remove(`show`);
-        })
+        dropdownItem.classList.toggle(`open-list`);
 
 
     }
 
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add(`active`);
-        tabsContent[i].classList.remove(`hide`);
-        tabs[i].classList.add(`show`);
-    }
+    //     // function openNavList() {
+    //     //     openListNav.classList.add(`open-list`);
+    //     // }
+
+    //     //     const tabs = document.querySelectorAll(`.js-payingItem`);
+    //     //     const tabsContent = document.querySelectorAll(`.js-serviceItem`);
+    //     //     const tabParent = document.querySelector(`.js-payingList`)
+
+    //     //     hideTabContent();
+    //     //     showTabContent();
+
+    //     //     tabParent.addEventListener(`click`, (event) => {
+    //     //         const target = event.target;
+    //     //         if (target && target.classList.contains(`js-payingItem`)) {
+    //     //             tabs.forEach((item, i) => {
+
+    //     //                 if (target == item) {
+    //     //                     hideTabContent();
+    //     //                     showTabContent(i);
+    //     //                 }
+    //     //             })
+    //     //         }
+    //     //     })
+
+
+    //     //     function hideTabContent() {
+    //     //         tabsContent.forEach(item => {
+    //     //             item.classList.add(`hide`);
+    //     //             item.classList.remove(`active`);
+    //     //         });
+
+    //     //         tabs.forEach(item => {
+    //     //             item.classList.remove(`show`);
+    //     //         })
+
+
+    //     //     }
+
+    //     //     function showTabContent(i = 0) {
+    //     //         tabsContent[i].classList.add(`active`);
+    //     //         tabsContent[i].classList.remove(`hide`);
+    //     //         tabs[i].classList.add(`show`);
+    //     //     }
 
 
 
